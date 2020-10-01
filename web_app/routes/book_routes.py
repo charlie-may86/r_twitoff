6194da11b28ce853e6f83d4039b9b9e18cdfdd3e
Charlie May <camiv95@gmail.com>
2020-09-30 20:13:45 -0500

    I can add stuff to the database from the local web page

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,25 +1,38 @@
 # web_app/routes/book_routes.py
 
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from web_app.models import Book, db, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "The Harry Potter Series"},
-        {"id": 2, "title": "The First Law Series"},
-        {"id": 3, "title": "Enders Game"},
-    ]
+    # books = [
+    #     {"id": 1, "title": "The Harry Potter Series"},
+    #     {"id": 2, "title": "The First Law Series"},
+    #     {"id": 3, "title": "Enders Game"},
+    # ]
+
+    # SELECT * FROM books
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records) # this is optional but useful
+
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "The Harry Potter Series"},
-        {"id": 2, "title": "The First Law Series"},
-        {"id": 3, "title": "Enders Game"},
-    ]
+    # books = [
+    #     {"id": 1, "title": "The Harry Potter Series"},
+    #     {"id": 2, "title": "The First Law Series"},
+    #     {"id": 3, "title": "Enders Game"},
+    # ]
+
+    # SELECT * FROM books
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records) # this is optional but useful
+
     return render_template("books.html", message="Here's some fantasy and sci fi books I love", books=books)
 
 @book_routes.route("/books/new")
@@ -29,9 +42,15 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
+    
+    
+    # INSERT INTO books ...
+    new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
+
     return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
+        "message": "BOOK CREATED OK",
         "book": dict(request.form)
     })
     #flash(f"Book '{new_book.title}' created successfully!", "success")

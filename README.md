93ca8ad47e410a2fab3b20d72a62803aea9a7bbb
Charlie May <camiv95@gmail.com>
2020-09-30 19:59:53 -0500

    setup local database

--- README.md
+++ README.md
@@ -23,3 +23,17 @@ pipenv shell
 ```sh
 FLASK_APP=web_app flask run
 ```
+
+## To get a new html template
+1. open a new file in the form some_file.html
+2. type HTML and then select html:5 and hit tab
+3. Put your code inbetween the two </body> snipets
+
+## The commands I used to set the database up
+```sh
+FLASK_APP=web_app flask db init #> generates app/migrations dir
+
+# run both when changing the schema:
+FLASK_APP=web_app flask db migrate #> creates the db (with "alembic_version" table)
+FLASK_APP=web_app flask db upgrade #> creates the specified tables
+```
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -9,11 +9,23 @@ from flask import Flask
 By importing these routes, I am able to initialize each page
 when running my web_app.
 '''
+
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+# DATABASE_URL = "sqlite:///twitoff_dspt7_development.db" # using relative filepath
+DATABASE_URL = "sqlite:////Users/charliemay/Desktop/Lambda/DSPT7/r_twitoff/web_app/twitoff_mike.db"
+
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
